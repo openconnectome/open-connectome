bf3eb3a3afaa8063d6aa33aafe4d257ccf3c586f
Alex Eusman <alex.eusman@gmail.com>
2016-07-07 14:42:59 -0400

    Security Auth for Graphgen

--- django/ndgraph/views.py
+++ django/ndgraph/views.py
@@ -15,7 +15,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.core.files.base import ContentFile
 from wsgiref.util import FileWrapper
@@ -25,19 +25,39 @@ import re
 from contextlib import closing
 import tarfile
 
+
 from ndwserror import NDWSError
 import ndgraph
 import logging
 logger=logging.getLogger("neurodata")
 
+from nduser.models import Project
+from nduser.models import Dataset
+from nduser.models import Token
+from nduser.models import Channel
+
+@login_required(login_url='/nd/accounts/login/')
 def buildGraph (request, webargs):
     """Build a graph based on different arguments"""
+    import pdb; pdb.set_trace()
     try:
-        (file, filename) = ndgraph.genGraphRAMON (*((webargs.replace(',','/').split('/'))[0:-1]))
+        args = (webargs.replace(',','/').split('/'))[0:-1]
+        w_token = args[0]
+
+        if not request.user.is_superuser:
+            m_tokens = Token.objects.filter(user=request.user.id) | Token.objects.filter(public=1)
+            tokens = []
+            for v in m_tokens.values():
+                tokens.append(v['token_name'])
+            if w_token not in tokens:
+                raise NDWSError ("Token {} does not exist or you do not have\
+                                  sufficient permissions to access it.".format(w_token))
+
+        (file, filename) = ndgraph.genGraphRAMON (*args)
         response = HttpResponse(content_type='text/plain')
         response['Content-Disposition'] = "attachment; filename=\"output.{}\"".format(filename)
         response.write(file.read())
         return response
     except Exception as e:
         logger.warning(e)
-        raise NDWSError(e)
+        raise e
