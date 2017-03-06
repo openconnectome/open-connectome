9c1ed239761b92d859d5a5438c775d904ffbe8af
Alex Eusman <alex.eusman@gmail.com>
2017-03-06 16:28:38 -0500

    Adding new authentication class

--- django/ndauth/authentication.py
+++ django/ndauth/authentication.py
@@ -6,12 +6,44 @@ from rest_framework import exceptions
 from nduser.models import Token
 
 from django.contrib.auth import authenticate
-#from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.models import Token as AuthToken
 from django.http import HttpResponseForbidden
 
 from webservices.ndwserror import NDWSError
 import logging
 logger=logging.getLogger("neurodata")
+
+
+
+class AnonAllowedAuthentication(authentication.TokenAuthentication):
+    def authenticate(self, request):
+        auth = get_authorization_header(request).split()
+
+        if not auth or auth[0].lower() != self.keyword.lower().encode():
+            return None
+
+        if len(auth) == 1:
+            msg = _('Invalid token header. No credentials provided.')
+            raise exceptions.AuthenticationFailed(msg)
+        elif len(auth) > 2:
+            msg = _('Invalid token header. Token string should not contain spaces.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        try:
+            token = auth[1].decode()
+        except UnicodeError:
+            msg = _('Invalid token header. Token string should not contain invalid characters.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        model = AuthToken
+        try:
+            token = model.objects.select_related('user').get(key=key)
+        except model.DoesNotExist:
+            return None
+
+        return (token.user, token)
+
+
 
 class PublicAuthentication(authentication.BaseAuthentication):
   
